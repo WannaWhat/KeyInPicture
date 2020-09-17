cf832417526014564e4f11ffe70deee3cd525c26
wannaWhat <352782Tt>
2020-09-17 19:38:15 +0300

    Add project skeleton

--- keyInPicture.py
+++ keyInPicture.py
@@ -8,6 +8,8 @@ class Picture():
 			sys.exit(-1)
 		self.FILE_NAME = args[1]
 		self.WORDS = args[2]
+		self.BEGIN = "1" * 17
+		self.END = "0" * 17
 
 	def checkImg(self):
 		try:
@@ -17,14 +19,34 @@ class Picture():
 			sys.exit(-2)
 
 	def cipherGenerator(self):
+
 		byte_array = self.WORDS.encode()
 
 		binary_int = int.from_bytes(byte_array, "big")
-		binary_string = bin(binary_int)
+		self.binary_string = bin(binary_int)[2::]
+		self.binary_string
 
-		print(binary_string)
+	def createTrack(self):
+		self.track = []
+		for i in self.BEGIN:
+			self.track.append(int(i))
+		for i in self.binary_string:
+			self.track.append(i)
+		for i in self.END:
+			self.track.append(int(i))
+
+	def getImg(self):
+		self.originIMG = Image.open(self.FILE_NAME)
+		self.originIMG.show()
+
+	def main(self):
+		self.checkImg()
+		self.cipherGenerator()
+		self.createTrack()
+		print(self.track)
+		self.getImg()
 
 
 if __name__ == "__main__":
 	pic = Picture(sys.argv)
-	print(pic.cipherGenerator())
+	pic.main()
