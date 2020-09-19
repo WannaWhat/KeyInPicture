be9d949a2e76e2004c0bb2834ec5279ec816912b
wannaWhat <352782Tt>
2020-09-19 10:34:37 +0300

    final version without How to use in readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # KeyInPicture
+
+
+## Installing:
+
+	git clone https://github.com/wannaWhat/KeyInPicture.git
+	cd KeyInPicture
+	pip install -r requirements.txt
+	python KeyInPicture.py []
+
+## How to use:
--- keyInPicture.py
+++ keyInPicture.py
@@ -1,15 +1,17 @@
-from PIL import Image
-import sys
+from PIL import Image, ImageDraw
+import binascii
+import argparse
 
 class Picture():
 	def __init__(self, args):
-		if len(args) < 3:
-			print("Bad argument!!!")
-			sys.exit(-1)
-		self.FILE_NAME = args[1]
-		self.WORDS = args[2]
-		self.BEGIN = "1" * 17
-		self.END = "0" * 17
+		self.FILE_NAME = args.inPhoto
+		if args.outPhoto:
+			self.WORDS = args.secretWord
+			self.OUTPHOTO = args.outPhoto
+			self.MODE = "code"
+		else:
+			self.SECRETCODE = args.password.split(" ")
+			self.MODE = "decode"
 
 	def checkImg(self):
 		try:
@@ -18,35 +20,101 @@ class Picture():
 			print("Bad Picture!!!")
 			sys.exit(-2)
 
-	def cipherGenerator(self):
-
-		byte_array = self.WORDS.encode()
-
-		binary_int = int.from_bytes(byte_array, "big")
-		self.binary_string = bin(binary_int)[2::]
-		self.binary_string
+	def cipherCoder(self, encoding='utf-8', errors='surrogatepass'):
+		text = self.WORDS
+		bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+		self.binary_string = bits.zfill(8 * ((len(bits) + 7) // 8))
 
 	def createTrack(self):
 		self.track = []
-		for i in self.BEGIN:
-			self.track.append(int(i))
 		for i in self.binary_string:
-			self.track.append(i)
-		for i in self.END:
 			self.track.append(int(i))
 
 	def getImg(self):
 		self.originIMG = Image.open(self.FILE_NAME)
-		self.originIMG.show()
+		self.SIZE = self.originIMG.size
+		self.PIX = self.originIMG.load()
+		self.NEWPIX = {}
+		for x in range(1, self.SIZE[0]):
+			for y in range(1, self.SIZE[1]):
+				self.NEWPIX[x, y] = self.PIX[x, y]
+
+	def getTrack(self):
+		self.originIMG = Image.open(self.FILE_NAME)
+		self.SIZE = self.originIMG.size
+		self.PIX = self.originIMG.load()
+		self.NEWPIX = {}
+		self.track = []
+		for x in range(1, self.SIZE[0]):
+			for y in range(1, self.SIZE[1]):
+				for pix in range(3):
+					self.track.append(self.PIX[x, y][pix] % 2)
+
+	def drawNewPix(self):
+		messChecker = 0
+		for x in range(1, self.SIZE[0]):
+			for y in range(1, self.SIZE[1]):
+				pix = []
+				for poz in range(3):
+					if messChecker < len(self.track): 
+						if self.NEWPIX[x, y][poz] % 2 != self.track[messChecker]:
+							if self.NEWPIX[x, y][poz] == 255:
+								pix.append(self.NEWPIX[x, y][poz] - 1)
+							else:
+								pix.append(self.NEWPIX[x, y][poz] + 1)
+						else:
+							pix.append(self.NEWPIX[x, y][poz])
+					else:
+						pix.append(self.NEWPIX[x, y][poz])
+					messChecker += 1
+				self.DRAW.point((x, y), (pix[0], pix[1], pix[2]))
+		self.NEWIGM.save("exmpl.png")
+
+	def newImage(self):
+		self.NEWIGM = Image.new("RGB", self.SIZE)
+		self.DRAW = ImageDraw.Draw(self.NEWIGM)
+
+	def decodeTrack(self):
+		code = self.track[int(self.SECRETCODE[0]):int(self.SECRETCODE[1]):]
+		secret_code = "0b"
+		for i in code:
+			secret_code += str(i)
+		n = int(secret_code, 2)
+		print(str(binascii.unhexlify('%x' % n))[1::])
+
+	def crypt(self):
+		self.checkImg()
+		self.cipherCoder()
+		self.createTrack()
+		print("Secret code: 0, ", len(self.track))
+		self.getImg()
+		self.newImage()
+		self.drawNewPix()
+
+	def uncrypt(self):
+		self.checkImg()
+		self.getTrack()
+		self.decodeTrack()
 
 	def main(self):
-		self.checkImg()
-		self.cipherGenerator()
-		self.createTrack()
-		print(self.track)
-		self.getImg()
+		if self.MODE == "code":
+			print("code")
+			self.crypt()
+
+		elif self.MODE == "decode":
+			print("decode")
+			self.uncrypt()
+
+
 
 
 if __name__ == "__main__":
-	pic = Picture(sys.argv)
+	parser = argparse.ArgumentParser(description='Image script')
+	parser.add_argument(action = "store", dest = "inPhoto", help = "Input photo")
+	parser.add_argument('-o', action = "store", dest = "outPhoto", help = "Output photo") 
+	parser.add_argument('-s', action = "store", dest = "secretWord", help = "Message for crypt")
+	parser.add_argument('-p', action = "store", dest = "password", help = "Password for decode")
+	args = parser.parse_args()
+
+	pic = Picture(args)
 	pic.main()
